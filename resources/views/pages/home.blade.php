fdf0b7092345cc76312e22f02f85b67869d4c66c
adarsh25t <adarsh25t@gmail.com>
2022-02-16 00:32:42 +0530

    home page section add

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,42 @@
 @extends('layouts.app')
 
+@include('sections.navbar')
 @section('home')
-<div>
-    
+
+<div class="hero">
+    <div class="shadow"></div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="{{ asset('images/bg-1.jpg') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="title">Chops & Hops</h5>
+              <p class="sub-title">THE BEST EXPERIENCE</p>
+              <button class="booking-table">Book Your Table</button>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('images/bg-2.jpg') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5 class="title">Chops & Hops</h5>
+                <p class="sub-title">THE BEST EXPERIENCE</p>
+                <button class="booking-table">View Your Menu</button>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('images/bg-3.jpg') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5 class="title">Chops & Hops</h5>
+                <p class="sub-title">THE BEST EXPERIENCE</p>
+                <button class="booking-table">Contact Me</button>
+              </div>
+          </div>
+        </div>
+      </div>
 </div>
 @endsection
--- resources/views/sections/navbar.blade.php
+++ resources/views/sections/navbar.blade.php
@@ -1,2 +1,15 @@
 @extends('layouts.app')
 
+<nav>
+    <h3>Chops & Hops</h3>
+    <ul>
+        <li><a href="/" class="active">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Chef</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+    <div class="authentication">
+        <button><a href="">Login</a></button>
+    </div>
+</nav>
